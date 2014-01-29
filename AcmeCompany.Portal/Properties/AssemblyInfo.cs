5176e8d633bc33e47677e85882252e818ea996d6
Maarten Balliauw <maarten@maartenballiauw.be>
2014-01-29 11:51:43 +0100

    Updated AssemblyInfo

--- AcmeCompany.Portal/Properties/AssemblyInfo.cs
+++ AcmeCompany.Portal/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AcmeCompany.Portal")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Company portal for AcmeCompany. Sample, Hello World MVC5 application")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AcmeCompany")]
 [assembly: AssemblyProduct("AcmeCompany.Portal")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © AcmeCompany 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
