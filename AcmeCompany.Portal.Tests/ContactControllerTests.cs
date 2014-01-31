bc4cc8a8e64557fc78702d23f6bcbef719bf136b
Maarten Balliauw <maarten@maartenballiauw.be>
2014-01-31 11:39:00 +0100

    Added some additional test cases

---
+++ AcmeCompany.Portal.Tests/ContactControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace AcmeCompany.Portal.Tests
+{
+    public class ContactControllerTests
+    {
+        [Test]
+        public void TestFormSubmission()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
