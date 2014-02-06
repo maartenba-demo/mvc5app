6d543127929e1e27078ccded09ffade934225d13
Maarten Balliauw <maarten@maartenballiauw.be>
2014-02-06 10:45:39 +0100

    Added deployment configurations

---
+++ AcmeCompany.Portal.Tests/AboutControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AcmeCompany.Portal.Tests
+{
+    public class AboutControllerTests
+    {
+        [Test]
+        public void TestContainsContactDetails()
+        {
+            Assert.IsTrue(true);
+        }
+        
+
+        [Test]
+        public void TestSomethingElse()
+        {
+            // Making the boss happy
+            // by adding more unit tests.
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ AcmeCompany.Portal.Tests/HomeControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace AcmeCompany.Portal.Tests
+{
+    public class HomeControllerTests
+    {
+        [Test]
+        public void TestHomepageContainsAcmeCompanyHeader()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void TestHomepageContainsLinkToAboutPage()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
